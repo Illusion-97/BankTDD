2bd3181f386c608e50e4150a360c08e7ed89127c
Yanis Adekalom <yanis971.60@gmail.com>
2023-11-30 15:34:45 +0100

    Initial commit

---
+++ src/test/java/fr/dawan/banktdd/BankTddApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.banktdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
