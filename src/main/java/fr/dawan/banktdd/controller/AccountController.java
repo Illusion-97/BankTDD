e1a17564341c1804dbf177f97372bae8f38e4054
Yanis Adekalom <yanis971.60@gmail.com>
2023-11-30 16:16:43 +0100

    Feat : GetAll

--- src/main/java/fr/dawan/banktdd/controller/AccountController.java
+++ src/main/java/fr/dawan/banktdd/controller/AccountController.java
@@ -1,11 +1,22 @@
 package fr.dawan.banktdd.controller;
 
+import fr.dawan.banktdd.dtos.AccountDto;
 import fr.dawan.banktdd.services.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/accounts")
 public class AccountController {
     private final AccountService service;
+
+    @GetMapping
+    public List<AccountDto> getAll() {
+        return service.getAll();
+    }
 }
